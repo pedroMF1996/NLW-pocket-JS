b03622fff14780405804b2f04d7b992a4f7de981
pmfrp <pmfrp@hotmail.com>
2024-09-09 23:20:29 -0300

    reordenando a estrutura de pastas e iniciando minha primeira function

---
+++ serve/src/db/seed.ts
@@ -0,0 +1,27 @@
+import dayjs from "dayjs";
+import { client, db } from ".";
+import { goalCompletions, goals } from "./schema";
+
+async function seed() {
+	await db.delete(goalCompletions);
+	await db.delete(goals);
+
+	const result = await db
+		.insert(goals)
+		.values([
+			{ title: "Acordar cedo", desiredWeeklyFrequency: 5 },
+			{ title: "Meditar", desiredWeeklyFrequency: 3 },
+			{ title: "Me exercitar", desiredWeeklyFrequency: 4 },
+		])
+		.returning();
+
+	const startOfWeek = dayjs().startOf("week");
+	await db.insert(goalCompletions).values([
+		{ goalId: result[0].id, createdAt: startOfWeek.toDate() },
+		{ goalId: result[0].id, createdAt: startOfWeek.add(1, "day").toDate() },
+	]);
+}
+
+seed().finally(() => {
+	client.end();
+});
